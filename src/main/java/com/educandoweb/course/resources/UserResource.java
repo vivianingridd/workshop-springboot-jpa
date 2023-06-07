eeccbe3e27feee4f9e6536e1521f87f171007f7d
vivianingridd <vivianingrid33@gmail.com>
2023-06-06 22:23:37 -0300

    User entity and resource

---
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.educandoweb.course.resources;
+
+import com.educandoweb.course.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findaAll() {
+        User u = new User(1, "Maria", "Maria@gmail", "99999999999", "1234");
+        return ResponseEntity.ok().body(u);
+    }
+
+}
